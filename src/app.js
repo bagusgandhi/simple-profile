89eba11ac3ca92202a4cd0d3db59ee64de91fe07
bagusgandhi <bagusgandhi4@gmail.com>
2022-06-18 13:12:56 +0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 app.set('views', './src/views'); // set views pada direktori ./src/views
 app.set('view engine', 'ejs'); // set view engine menggunakan EJS
-app.use(express.static('./src/public')); // add express static in public dir
+// app.use(express.static('./src/public')); // add express static in public dir
 
 app.get('/', (req, res) => {
    res.render('index');
